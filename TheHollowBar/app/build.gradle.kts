e3a4bc884583c05b4c5dc2a2b7f06081d4a9c578
Branditon <brandonescudero2810@gmail.com>
2024-09-16 22:11:42 -0500

    action button

--- TheHollowBar/app/build.gradle.kts
+++ TheHollowBar/app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application")
+    id("org.jetbrains.kotlin.android") version "1.8.22"
 }
 
 android {
@@ -25,17 +26,22 @@ android {
             )
         }
     }
+
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
     }
 }
 
 dependencies {
-
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.activity:activity:1.9.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
--- TheHollowBar/app/src/main/java/com/example/thehollowbar/Home.java
+++ TheHollowBar/app/src/main/java/com/example/thehollowbar/Home.java
@@ -1,8 +1,12 @@
 package com.example.thehollowbar;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.PopupMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class Home extends AppCompatActivity {
 
@@ -10,5 +14,34 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Button buttonLogin = findViewById(R.id.buttonLogin);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                showPopupMenu(v);
+            }
+        });
+    }
+
+    private void showPopupMenu(View view) {
+        PopupMenu popupMenu = new PopupMenu(this, view);
+
+        MenuInflater inflater = popupMenu.getMenuInflater();
+        inflater.inflate(R.menu.popup_menu, popupMenu.getMenu());
+
+        //popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+
+            //public boolean onMenuItemClick(MenuItem item) {
+                //switch (item.getItemId()) {
+                    //case R.id.optionLogin:
+                        //return true;
+                    //default:
+                        //return false;
+                //}
+            //}
+        //});
+
+        popupMenu.show();
     }
 }
