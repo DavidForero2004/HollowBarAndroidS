bce3046298b7ad0ccdc89f3d9dc03598576e44a9
Branditon <brandonescudero2810@gmail.com>
2024-09-18 14:46:26 -0500

    Login Client

--- TheHollowBar/app/src/main/java/com/example/thehollowbar/LoginClient.java
+++ TheHollowBar/app/src/main/java/com/example/thehollowbar/LoginClient.java
@@ -2,8 +2,10 @@ package com.example.thehollowbar;
 
 import android.os.Bundle;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -16,9 +18,32 @@ public class LoginClient extends AppCompatActivity {
         setContentView(R.layout.activity_login_client);
 
         ImageButton buttonBack = findViewById(R.id.buttonBackClient);
+        TextView selectTypeDocument = findViewById(R.id.selectTypeDocument);
+        TextView selectTable = findViewById(R.id.selectTable);
+
+        final String[] optionsTypeDocument = getResources().getStringArray(R.array.options_array_type_document);
+        final String[] optionsTable = getResources().getStringArray(R.array.options_array_table);
 
         buttonBack.setOnClickListener(v -> {
             onBackPressed();
+        });
+
+        selectTypeDocument.setOnClickListener(v -> {
+            AlertDialog.Builder builder = new AlertDialog.Builder(LoginClient.this);
+            builder.setTitle("Tipo de documento");
+            builder.setItems(optionsTypeDocument, (dialog, which) -> {
+                selectTypeDocument.setText(optionsTypeDocument[which]);
+            });
+            builder.show();
+        });
+
+        selectTable.setOnClickListener(v -> {
+            AlertDialog.Builder builder = new AlertDialog.Builder(LoginClient.this);
+            builder.setTitle("Mesa");
+            builder.setItems(optionsTable, (dialog, which) -> {
+                selectTable.setText(optionsTable[which]);
+            });
+            builder.show();
         });
     }
 }
