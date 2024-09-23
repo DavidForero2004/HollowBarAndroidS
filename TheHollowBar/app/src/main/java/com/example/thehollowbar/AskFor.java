ca6eb654fe1c329fd38304ef31babff1f2a798fe
DavidForero2004 <davidforeromoreno2016@gmail.com>
2024-09-22 21:54:24 -0500

    form-askfor

--- TheHollowBar/app/src/main/java/com/example/thehollowbar/AskFor.java
+++ TheHollowBar/app/src/main/java/com/example/thehollowbar/AskFor.java
@@ -7,14 +7,43 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.widget.TextView;
+import android.widget.Button;
+
+
 
 public class AskFor extends AppCompatActivity {
+
+    private TextView productQuantity;
+    private int quantity = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ask_for);
 
+        productQuantity = findViewById(R.id.productQuantity);
+        Button buttonAdd = findViewById(R.id.btnAddition);
+        Button buttonSubtract = findViewById(R.id.btnSubtraction);
 
+        buttonAdd.setOnClickListener(v -> add());
+        buttonSubtract.setOnClickListener(v -> subtract());
+
+    }
+
+    private void add() {
+        quantity++;
+        updateQuantity();
+    }
+
+    private void subtract() {
+        if (quantity > 1) {
+            quantity--;
+        }
+        updateQuantity();
+    }
+
+    private void updateQuantity() {
+        productQuantity.setText(String.valueOf(quantity));
     }
 }
--- TheHollowBar/app/src/main/java/com/example/thehollowbar/Home.java
+++ TheHollowBar/app/src/main/java/com/example/thehollowbar/Home.java
@@ -45,5 +45,6 @@ public class Home extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
     }
 }
